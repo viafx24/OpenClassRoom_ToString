8f984df5ccf688348469f6e243d3dab4320c58e6
Guillaume Baptist <guillaume.baptist@free.fr>
2020-12-29 20:37:43 +0100

    Ajoutez des fichiers projet.

---
+++ OpenClassRoom_ToString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OpenClassRoom_ToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObjetVide monObjetVide = new ObjetVide();
+            Chien chien = new Chien();
+            int age = 30;
+            string prenom = "Nicolas";
+
+            AfficherRepresentation(monObjetVide);
+            AfficherRepresentation(chien);
+            AfficherRepresentation(age);
+            AfficherRepresentation(prenom);
+        }
+
+        private static void AfficherRepresentation(object monObjetVide)
+        {
+            Console.WriteLine(monObjetVide.ToString());
+        }
+    }
+}
